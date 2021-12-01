97e43cb23e57926119b8c432037b01ee47b49e27
Anguadarrama <75002100+Anguadarrama@users.noreply.github.com>
2021-12-01 06:10:56 -0600

    Add files via upload
    Final Project for my Intermediate Web Design class in school

---
+++ script.js
@@ -0,0 +1,29 @@
+// This is my script used for my form to print out a confirmation windows
+
+"use strict";
+
+function processInput() {
+  var propertyWidth = 300;
+  var propertyHeight = 100;
+  var winLeft = ((screen.width - propertyWidth) / 2);
+  var winTop = ((screen.height - propertyHeight) / 2);
+  var winOptions = "width=300,height=100";
+  winOptions += ",left=" + winLeft;
+  winOptions += ",top=" + winTop;
+  window.open("confirm.htm", "confirm", winOptions);
+}
+
+function createEventListener() {
+  var submitButton = document.getElementById("submitBtn");
+  if (submitButton.addEventListener) {
+    submitButton.addEventListener("click", processInput, false);
+  } else if (submitButton.attachEvent) {
+    submitButton.attachEvent("onclick", processInput);
+  }
+}
+
+if (window.addEventListener) {
+  window.addEventListener("load", createEventListener, false);
+} else if (window.attachEvent) {
+  window.attachEvent("onload", createEventListener);
+}
